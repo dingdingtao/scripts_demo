72bdea7aad0861419db3ed57f62a2fc0a8f7fe5c
dingtao <m18216041426@163.com>
2021-03-24 18:55:33 +0800

    1

---
+++ python/test/urlencode_test/urlencode_test.py
@@ -0,0 +1,12 @@
+'''
+Author: dingdingtao
+Date: 2021-03-23 17:45:15
+LastEditTime: 2021-03-23 17:45:30
+LastEditors: dingdingtao
+Description: 
+'''
+from urllib.parse import urlencode
+
+
+def run():
+    pass
